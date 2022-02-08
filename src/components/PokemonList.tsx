ddcc0df2faf74bf71f5a6441ff6a8d59e220fd0d
Willy Alers <willy.alers@allheart.net>
2022-02-08 11:57:29 -0800

    Moved images to public folder.

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,11 +1,12 @@
 import classes from './PokemonList.module.css';
 import { Pokemon } from '../model/Pokemon';
 import { GetPokemonList } from '../utils/GetPokemon';
+import Card from './Card';
 
 export const PokemonList = () => {
     const pokemonList:Pokemon[] = GetPokemonList();
 
     return <div className={classes['pokemon-list']}>
-        { pokemonList.map(pokemon => <li>{ pokemon.name }</li>) }
+        { pokemonList.map(pokemon => <Card {...pokemon} />) }
     </div>
 }
