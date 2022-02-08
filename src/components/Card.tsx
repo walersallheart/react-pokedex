45c17a9aba68b43b94db6a4ecb1eb5573974c66f
Willy Alers <willy.alers@allheart.net>
2022-02-08 13:11:48 -0800

    Adjusted grid colors and spacing.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,7 +7,7 @@ const Card:React.FC<Pokemon> = (props)  => {
 
     return <div className={classes.card} style={{backgroundColor:typeColor, backgroundImage: `url('images/thumbnails/${props.id}.png')`}}>
         <p className={classes['pokemon-name']}>{props.name}</p>
-        <ul className="type-list">
+        <ul className={classes['type-list']}>
             {props.type?.map(type => <li>{type}</li>)}
         </ul>
     </div>;
