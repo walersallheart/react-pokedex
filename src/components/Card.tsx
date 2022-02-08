3384e8ca6f28db3ba123a865404f749bd61bbc7b
Willy Alers <willy.alers@allheart.net>
2022-02-08 12:49:20 -0800

    Updated text styling.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,7 +6,7 @@ const Card:React.FC<Pokemon> = (props)  => {
     const typeColor = GetPokemonTypeColor(Number(props.id));
 
     return <div className={classes.card} style={{backgroundColor:typeColor, backgroundImage: `url('images/thumbnails/${props.id}.png')`}}>
-        <p>{props.name}</p>
+        <p className={classes['pokemon-name']}>{props.name}</p>
         <ul className="type-list">
             {props.type?.map(type => <li>{type}</li>)}
         </ul>
