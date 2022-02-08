060ab98de056111be385412961f73b45a845371c
Willy Alers <willy.alers@allheart.net>
2022-02-08 13:53:22 -0800

    Added correct link functionality and styling.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,19 @@
 import classes from './Card.module.css';
 import { GetPokemonTypeColor } from "../utils/GetPokemon";
+import { Link } from 'react-router-dom';
 
 const Card = (props:any)  => {
     const pokemon = props.pokemon;
     const typeColor = GetPokemonTypeColor(Number(pokemon.id));
+    const detailPageLink = `pokemon/${pokemon.id}`;
 
-    const selectPokemon = () =>  {
-        console.log('selectPokemon');
-    }
-
-    return <div onClick={selectPokemon} className={classes.card} style={{backgroundColor:typeColor, backgroundImage: `url('images/thumbnails/${pokemon.id}.png')`}}>
-        <p className={classes['pokemon-name']}>{pokemon.name}</p>
-        <ul className={classes['type-list']}>
-            {pokemon.type?.map((type:string) => <li key={type}>{type}</li>)}
-        </ul>
+    return <div className={classes.card} style={{backgroundColor:typeColor, backgroundImage: `url('images/thumbnails/${pokemon.id}.png')`}}>
+        <Link to={detailPageLink}>
+            <p className={classes['pokemon-name']}>{pokemon.name}</p>
+            <ul className={classes['type-list']}>
+                {pokemon.type?.map((type:string) => <li key={type}>{type}</li>)}
+            </ul>
+        </Link>
     </div>;
 }
 
