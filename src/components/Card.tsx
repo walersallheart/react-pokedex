8d00e5fdc58c3e3291749431a40c04f16fbf318d
Willy Alers <willy.alers@allheart.net>
2022-02-08 16:45:36 -0800

    Added lazy loading.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,26 @@
 import classes from './Card.module.css';
 import { GetPokemonTypeColor } from "../utils/GetPokemon";
 import { Link } from 'react-router-dom';
+import LazyLoad from 'react-lazyload';
 
 const Card = (props:any)  => {
     const pokemon = props.pokemon;
     const typeColor = GetPokemonTypeColor(Number(pokemon.id));
     const detailPageLink = `pokemon/${pokemon.id}`;
 
-    return <div className={classes.card} style={{backgroundColor:typeColor, backgroundImage: `url('images/thumbnails/${pokemon.id}.png')`}}>
+    return <div className={classes.card} style={{backgroundColor:typeColor}}>
         <Link to={detailPageLink}>
             <p className={classes['pokemon-name']}>{pokemon.name}</p>
             <ul className={classes['type-list']}>
                 {pokemon.type?.map((type:string) => <li key={type}>{type}</li>)}
             </ul>
+            <LazyLoad height={100} once>
+                <img
+                    className={classes.thumbnail}
+                    src={`images/thumbnails/${pokemon.id}.png`}
+                    alt={pokemon.name}
+                />
+            </LazyLoad>
         </Link>
     </div>;
 }
